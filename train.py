d35248f83daea508d3357b4c05b884839c617100
Ridouan EL KALLALI <r.elkallali@gmail.com>
2021-03-03 02:12:01 +0100

    Update train.py

--- train.py
+++ train.py
@@ -11,8 +11,42 @@ from collections import OrderedDict
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from devil import save_checkpoint, load_checkpoint
+import copy
+import os
+import json
 
+def save_checkpoint(path, model, optimizer, args, classifier):
+    
+    checkpoint = {'arch': args.arch, 
+                  'model': model,
+                  'learning_rate': args.learning_rate,
+                  'hidden_units': args.hidden_units,
+                  'classifier' : classifier,
+                  'epochs': args.epochs,
+                  'optimizer': optimizer.state_dict(),
+                  'state_dict': model.state_dict(),
+                  'class_to_idx': model.class_to_idx}
+
+    torch.save(checkpoint, path)
+    
+    
+def load_checkpoint(filepath):
+    checkpoint = torch.load(filepath)
+    model = checkpoint['model']
+    model.classifier = checkpoint['classifier']
+    learning_rate = checkpoint['learning_rate']
+    epochs = checkpoint['epochs']
+    optimizer = checkpoint['optimizer']
+    model.load_state_dict(checkpoint['state_dict'])
+    model.class_to_idx = checkpoint['class_to_idx']
+    
+    return model
+
+def load_cat_names(filename):
+    with open(filename) as f:
+        category_names = json.load(f)
+    return category_names
+    
 def parse_args():
     parser = argparse.ArgumentParser(description="Training")
     parser.add_argument('--data_dir', action='store')
@@ -92,6 +126,7 @@ def main():
     validataion_transforms = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(),
                                                  transforms.Normalize([0.485, 0.456, 0.406], 
                                                                       [0.229, 0.224, 0.225])])
+
     testing_transforms = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(),
                                              transforms.Normalize([0.485, 0.456, 0.406], 
                                                                   [0.229, 0.224, 0.225])]) 
